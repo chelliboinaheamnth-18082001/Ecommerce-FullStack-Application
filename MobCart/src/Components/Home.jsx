7b1445b1874304288995ce677be92ac2ae71748c
chelliboinaheamnth-18082001 <chelliboinahemanth.18082001@gmail.com>
2026-06-17 20:15:34 +0530

    Implemented the Home,ProductCard,productListing,DropDown,SearchBox components

--- MobCart/src/Components/Home.jsx
+++ MobCart/src/Components/Home.jsx
@@ -1,9 +1,49 @@
-import products from "../data/products";
+import apiClient from "../API/apiClient";
 import PageHeading from "./PageHeading";
 import ProductListings from "./ProductsListing";
+import { useState, useEffect } from "react";
+
 export default function Home() {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  const fetchProducts = async () => {
+    try {
+      setLoading(true);
+      const response = await apiClient.get("/products"); // Axios GET Request
+      setProducts(response.data); // Update products state with fetched data
+    } catch (error) {
+      setError(
+        error.response?.data?.message ||
+          "Failed to fetch products. Please try again.",
+      ); // Extract error message if available
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        <span className="text-xl font-semibold">Loading products...</span>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        <span className="text-xl text-red-500">Error: {error}</span>
+      </div>
+    );
+  }
   return (
-    <div className="home-container">
+    <div className="max-w-[1152px] mx-auto px-6 py-8">
       <PageHeading title="Explore The Latest Mobiles">
         One Stop Shop For All Your Mobile Needs AND Gadgets Make Your Life
         Easier
